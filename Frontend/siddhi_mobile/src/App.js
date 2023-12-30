fc56c03f6dc67c3c1fc9078d3bf0d841ac47670f
linal <linalpatil72@gmail.com>
2023-12-30 20:08:01 +0530

    Second commit

--- Frontend/siddhi_mobile/src/App.js
+++ Frontend/siddhi_mobile/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import Footer from './components/Footer';
 import Form from './components/Form';
 import Header from './components/Header';
+import Landing from './components/Landing';
 function App() {
   return (
     <>
     <Header></Header>
-      <Form></Form>
+    <Form></Form>
+      <Landing></Landing>
       <Footer></Footer>
       
     </>
---
+++ Frontend/siddhi_mobile/src/components/Landing.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Header from './Header'
+export default function Landing() {
+  return (
+    <div className='container'>
+      <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSA4YH-lpAGdtyF1K_wEVRJqO0MWYVRZiiGdA&usqp=CAU'/>
+    </div>
+  )
+}
