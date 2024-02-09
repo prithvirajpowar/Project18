9b294a11c5b08db60619745e781d5bc3f21cd484
prithvi <prithvirajpowar25@gmail.com>
2024-02-09 22:48:42 +0530

    Backend successful

--- Backend/project.siddhimobiles/src/main/java/com/review_api/repository/EnquiryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.review_api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.siddhimobiles.entity.Enquiry;
-
-public interface EnquiryRepository extends JpaRepository<Enquiry, Integer> {
-
-}
