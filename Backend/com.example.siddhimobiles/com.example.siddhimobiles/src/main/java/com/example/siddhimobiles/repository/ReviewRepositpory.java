e47a58aa7dd1e37f8c7e519bde82a0015a5626b9
prithvi <prithvirajpowar25@gmail.com>
2024-02-25 14:35:40 +0530

    backend deleted

--- Backend/com.example.siddhimobiles/com.example.siddhimobiles/src/main/java/com/example/siddhimobiles/repository/ReviewRepositpory.java
+++
@@ -1,9 +0,0 @@
-package com.example.siddhimobiles.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.siddhimobiles.entity.Review;
-
-public interface ReviewRepositpory extends JpaRepository<Review, Long> {
-
-}
--- Backend/com.example.siddhimobiles/src/main/java/com/example/siddhimobiles/entity/Role.java
+++
@@ -1,5 +0,0 @@
-package com.example.siddhimobiles.entity;
-
-public enum Role {
-	USER, ADMIN
-}
